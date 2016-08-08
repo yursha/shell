58819b4094303ba07d8601ecd9cb4239c78b0b88
Alex Yursha <alexyursha@gmail.com>
2016-08-07 19:17:28 -0700

    cleanup

--- builtins/mkbuiltins.c
+++ builtins/mkbuiltins.c
@@ -1,24 +1,6 @@
 /* mkbuiltins.c - Create builtins.c, builtext.h, and builtdoc.c from
    a single source file called builtins.def. */
 
-/* Copyright (C) 1987-2016 Free Software Foundation, Inc.
-
-   This file is part of GNU Bash, the Bourne Again SHell.
-
-   Bash is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Bash is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Bash.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #if !defined(CROSS_COMPILING)
 #include <config.h>
 #else /* CROSS_COMPILING */
